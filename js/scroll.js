f226292ee21cd5478a63cc8b6e02c2d0f6da588e
Orlando Kuan <nexuscode25@gmail.com>
2021-04-14 20:51:57 -0500

    fix menu

--- js/scroll.js
+++ js/scroll.js
@@ -51,10 +51,11 @@ function navHighlighter() {
   
   // Now we loop through sections to get height, top and ID values for each
   sections.forEach(current => {
+    
     const sectionHeight = current.offsetHeight;
     const sectionTop = current.offsetTop - 50;
     sectionId = current.getAttribute("id");
-   
+    console.log(sectionId);
     /*
     - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
     - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
